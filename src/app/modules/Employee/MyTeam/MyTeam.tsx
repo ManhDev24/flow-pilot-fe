5a47ae62f6614a909d277970c06eb80177e1d44b
quadas <quadas897@gmail.com>
2025-10-08 19:17:34 +0700

    bat bai v4

--- src/app/modules/Employee/MyTeam/MyTeam.tsx
+++ src/app/modules/Employee/MyTeam/MyTeam.tsx
@@ -78,7 +78,7 @@ function MyTeam() {
       <div className='flex items-center justify-between'>
         <div>
           <h1 className='text-2xl font-bold text-gray-900'>My Team</h1>
-          <p className='text-gray-600 mt-1'>Manage and view your team members for project: {projectData.data?.name}</p>
+          <p className='text-gray-600 mt-1'>Manage and view your team members for project: {projectData?.data?.name}</p>
         </div>
       </div>
 
